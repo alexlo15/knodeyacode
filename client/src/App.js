0ed38261bee18d9c84cb8beb9ddfa1f71d39a91b
Hetal Patel <nphetal1905@gmail.com>
2019-08-29 12:21:35 -0400

    test

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Mainpage from "./components/Mainpage";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Main Page</h1>
+     <Mainpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Mainpage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Mainpage(){
+    return(
+        <div>
+        <h1>Knode Your Code</h1>
+        <button >Sign In</button>
+        </div>
+    );
+}
+
+export default Mainpage;
