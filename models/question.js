7b65dbcfe372409a012cc6ad0423420be40f6f75
Hetal Patel <nphetal1905@gmail.com>
2019-08-29 13:29:17 -0400

    connected mongodb

---
+++ models/question.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Defines the table of Question for the database
+const questionSchema = new Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  choice1: {
+    type: String,
+    required: true
+  },
+  choice2: {
+    type: String,
+    required: true
+  },
+  choice3: {
+    type: String,
+    required: true
+  }
+
+  // date: { type: Date, default: Date.now }
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+module.exports = Question;
+
+// ==================Forgeign key need to be setup=============================
+// Question.associate = function(models) {
+//     // Creates the foreign key from topics in questions
+//     Question.belongsTo(models.Topic, {
+//       foreignKey: {
+//         allowNull: false
+//       }
+//     });
+//   };
+
+ 
