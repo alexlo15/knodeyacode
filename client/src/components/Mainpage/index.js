e5232c528c202ee0083e897a9ae1eaaf9346c382
rishiajay500 <rishiajay500@gmail.com>
2019-08-29 13:43:59 -0400

    updated the css

--- client/src/components/Mainpage/index.js
+++ client/src/components/Mainpage/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./mainPage.css";
 
 function Mainpage(){
     return(
         <div>
         <h1>Knode Your Code</h1>
-        <button >Sign In</button>
+        <p>Please log in to play</p>
+
+        <button type="button" class="btn btn-primary">Sign in</button>
         </div>
     );
 }
