c75d6a74ccf309d943be99f0b59c12d4a5d70730
Alex Lo <loalexander389@gmail.com>
2019-08-29 14:11:15 -0400

    sorry im on the master

---
+++ scripts/scoreDB.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/knodyacode");
+
+const scoreSeed = [
+
+{
+    userName: "Ash Ketchum",
+    score: 15
+}
+    
+];
+
+db.Score
+  .remove({})
+  .then(() => db.Score.collection.insertMany(scoreSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
