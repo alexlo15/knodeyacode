7073f7c2c0faf9af5c4ea9fa9485505a0a16af17
Hetal Patel <nphetal1905@gmail.com>
2019-09-03 12:57:08 -0400

    get dropped img name on click

--- client/src/components/ImgContainer/ImgContainer.js
+++ client/src/components/ImgContainer/ImgContainer.js
@@ -3,8 +3,10 @@ import React, { Component } from "react";
 import "./ImgContainer.css";
 import Img from "../Img/Img";
 // import imgList from "./imageList.json";
+let topicSelected = [];
 
 class ImgContainer extends Component {
+ 
   state = {
     images: [
       {
@@ -26,8 +28,11 @@ class ImgContainer extends Component {
         category: "notDragged"
       }
     ]
+   
   };
-
+  
+// This are drag and drop functionality
+// ====================================================
   onDragOver = ev => {
     ev.preventDefault();
   };
@@ -39,6 +44,7 @@ class ImgContainer extends Component {
 
   onDrop = (ev, cat) => {
     let id = ev.dataTransfer.getData("id");
+    topicSelected.push(id);
     let images = this.state.images.filter(img => {
       if (img.name === id) {
         img.category = cat;
@@ -52,12 +58,17 @@ class ImgContainer extends Component {
     });
     console.log("Dropped : " + id);
   };
-
+// ===========================================================
+onClick = ()=>{
+  // alert('click attached');
+  console.log(topicSelected);
+}
   render() {
     var images = {
       notDragged: [],
       dragged: []
     };
+   
 
     this.state.images.forEach(img => {
       images[img.category].push(
@@ -81,6 +92,7 @@ class ImgContainer extends Component {
     // ));
 
     return (
+      
       <main>
         <div>
           <h2>ImgContainer component</h2>
@@ -100,6 +112,9 @@ class ImgContainer extends Component {
           onDrop={e => this.onDrop(e, "dragged")}
         >
           {images.dragged}
+        </div>
+        <div id="btnDiv">
+          <button className="btn btn-primary btnSubmit" onClick={this.onClick}>Submit</button>
         </div>
       </main>
     );
