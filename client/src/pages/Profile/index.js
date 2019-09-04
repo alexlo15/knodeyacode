2ad382ba6c53a30657caeaaf5e6a78c6e361775d
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-09-04 15:09:59 -0400

    updating the profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Sidebar from "../../components/Sidebar";
 import "./style.css";
 
 function Profile() {
   return (
-    <div>
-        <h3>This is the profile page</h3>
+    <div className="Flashpage">
+    <div className="row">
+      <div className="col-12 col-sm-2">
+      <Sidebar />
+      </div>
     </div>
+    
+    
+  </div>
+
   );
 }
 
