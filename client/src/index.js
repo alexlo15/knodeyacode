03bc58215ae9802f2b487c1ffe0cd4638afa7410
Hetal Patel <nphetal1905@gmail.com>
2019-09-04 11:54:59 -0400

    flashCard page load on submit click

--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import DragDropPage from './dragDropPAGE';
 import FlashPage from './flashcardPAGE';
 import FlashCard from "./components/FlashCard";
+import NavBar from "./components/NavBar";
 // this is the main index.js file that handles routes
 
 const routing = (
     <Router>
+        <NavBar />
+        <Switch>
         <div>
             {/* these routes tell what component(PAGE)
             to load on each path */}
             <Route exact path="/" component={App} />
             <Route exact path="/flashcard" component={FlashPage} />
+            <Route exact path="/flashCardSubmit" component={FlashCard} />
             <Route exact path="/dragdrop" component={DragDropPage} />
             <Route exact path="/maintest" component={FlashPage} />
             <Route exact path={`/flashCardSubmit`} component={FlashCard} />
         </div>
+        </Switch>
     </Router>
 )
 
