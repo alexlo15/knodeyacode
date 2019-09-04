5330553f36f17b780c0a6468909bb1b3babd0857
Alex Lo <loalexander389@gmail.com>
2019-09-04 11:14:14 -0400

    flashcards

---
+++ client/src/components/Notecard/nextbutton.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './nextbutton.css';
+
+export default class DrawButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.drawCard = this.drawCard.bind(this);
+  }
+
+  drawCard() {
+    this.props.drawCard();
+  }
+
+  render(props) {
+    return (
+      <div className='button-container'>
+        <button className='button' onClick={this.drawCard}>Draw card</button>
+      </div>
+    );
+  }
+}
--- client/src/components/Notecard/notecard.js
+++ client/src/components/Notecard/notecard.js
@@ -1,23 +1,79 @@
-import React from "react";
-import "./style.css";
+import "./notecard.css";
+import React, { Component } from 'react';
+import Card from './card'
+import DrawButton from "./nextbutton";
 
-function Notecard() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark black">
-            <a className="navbar-brand" href="/">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-                    <a className="nav-item nav-link" href="/test">Test</a>
-                    <a className="nav-item nav-link" href="/dragdrop">DragDrop</a>
-                    <a className="nav-item nav-link disabled" href="/maintest" tabindex="-1" aria-disabled="true">Maintest</a>
+export default class Flashcards extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cards: [
+                {
+                    question: '404',
+                    choice1: 'Not found',
+                }
+            ],
+            currentCard: {}
+        }
+
+        this.updateCard = this.updateCard.bind(this);
+    }
+
+    //this is called a lifecycle hook
+    componentWillMount() {
+        const currentCards = this.state.cards;
+
+        //fetch JSON here!
+        fetch('https://raw.githubusercontent.com/alexlo15/knodeyacode/master/questions.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                data.forEach(function (item) {
+                    // let desc = item.description;
+                    // desc = desc.replace(/"/g, "");
+                    currentCards.push({
+                        question: item.question,
+                        choice1: item.choice1,
+                    });
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+
+        this.setState({
+            cards: currentCards,
+            currentCard: this.getRandomCard(currentCards)
+        });
+    }
+
+    getRandomCard(currentCards) {
+        let card = currentCards[Math.floor(Math.random() * currentCards.length)];
+        return card;
+    }
+
+    updateCard() {
+        const currentCards = this.state.cards;
+        this.setState({
+            currentCard: this.getRandomCard(currentCards)
+        })
+    }
+
+    render() {
+        return (
+            <div className='Flashcard'>
+                <div className='card-row'>
+                    <Card question={this.state.currentCard.question}
+                        answer={this.state.currentCard.choice1}
+                    //   description={this.state.currentCard.description} 
+                    />
+                </div>
+                <div className='button-row'>
+                    <DrawButton drawCard={this.updateCard} />
                 </div>
             </div>
-        </nav>
-    );
+        );
+    }
 }
-
-export default NavBar;
