db301fffcd5f3f5e845aa946e274c804eb83c621
Andrew <siricoa94@github.com>
2019-09-04 12:36:20 -0400

    cleaned up app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,30 @@
 import React from 'react';
-import NavBar from "./components/NavBar";
-import './styles/App.css';
-import Mainpage from './components/Mainpage';
-// ============================================
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import Mainpage from './components/Mainpage';
 import DragDropPage from './dragDropPAGE';
 import FlashPage from './flashcardPAGE';
 import FlashCard from "./components/FlashCard";
-import NavBar from "./components/NavBar";
-// this is the main index.js file that handles routes
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar />
-//       <Mainpage />
-//     </div>
-//   );
-// }
-const routing = (
-  <Router>
-      <NavBar />
-      <Switch>
-      <div>
-          {/* these routes tell what component(PAGE)
-          to load on each path */}
-          <Route exact path="/" component={App} />
-          <Route exact path="/flashcard" component={FlashPage} />
-          <Route exact path="/flashCardSubmit" component={FlashCard} />
-          <Route exact path="/dragdrop" component={DragDropPage} />
-          <Route exact path="/maintest" component={FlashPage} />
-          <Route exact path={`/flashCardSubmit`} component={FlashCard} />
-      </div>
-      </Switch>
-  </Router>
-)
-
-ReactDOM.render(routing, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Mainpage />
-      </div>
-
-    );
+      <Router>
+        <Switch>
+          <div>
+            {/* these routes tell what component(PAGE) to load on each path */}
+            <Route exact path="/" component={Mainpage} />
+            <Route exact path="/flashcard" component={FlashPage} />
+            <Route exact path="/flashCardSubmit" component={FlashCard} />
+            <Route exact path="/dragdrop" component={DragDropPage} />
+            <Route exact path="/maintest" component={FlashPage} />
+            <Route exact path={`/flashCardSubmit`} component={FlashCard} />
+          </div>
+        </Switch>
+      </Router>
+    )
   }
-}
 
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from ".App";
+import App from "./App";
 
-ReactDom.render(<App />, document.getElementBy("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
