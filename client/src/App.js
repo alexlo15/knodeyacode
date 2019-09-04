119b0a475844e21c683ce412d5d657eb02db554d
Andrew <siricoa94@github.com>
2019-09-04 12:06:01 -0400

    deleted not needed files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-// import logo from './logo.svg';
 import NavBar from "./components/NavBar";
 import './styles/App.css';
 import Mainpage from './components/Mainpage';
+// ============================================
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import DragDropPage from './dragDropPAGE';
+import FlashPage from './flashcardPAGE';
+import FlashCard from "./components/FlashCard";
+import NavBar from "./components/NavBar";
+// this is the main index.js file that handles routes
 
 // function App() {
 //   return (
@@ -12,7 +23,30 @@ import Mainpage from './components/Mainpage';
 //     </div>
 //   );
 // }
+const routing = (
+  <Router>
+      <NavBar />
+      <Switch>
+      <div>
+          {/* these routes tell what component(PAGE)
+          to load on each path */}
+          <Route exact path="/" component={App} />
+          <Route exact path="/flashcard" component={FlashPage} />
+          <Route exact path="/flashCardSubmit" component={FlashCard} />
+          <Route exact path="/dragdrop" component={DragDropPage} />
+          <Route exact path="/maintest" component={FlashPage} />
+          <Route exact path={`/flashCardSubmit`} component={FlashCard} />
+      </div>
+      </Switch>
+  </Router>
+)
 
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 class App extends React.Component {
 
