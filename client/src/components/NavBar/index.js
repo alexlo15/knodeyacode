5771d573295c31d377214d5596dc8af9db11109c
Alex Lo <loalexander389@gmail.com>
2019-09-05 10:26:03 -0400

    img logo

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark black">
 
-            <a className="navbar-brand logo" href="/"><img src={logo}/></a>
+            <a className="navbar-brand logo" href="/"><img src={logo} alt={logo}/></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
