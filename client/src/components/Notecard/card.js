f65e59c14b5af4717ba6bb5bc5e99405a420e1ff
Hetal Patel <nphetal1905@gmail.com>
2019-09-05 10:12:44 -0400

    dragdroppage data from db

--- client/src/components/Notecard/card.js
+++ client/src/components/Notecard/card.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import './card.css';
+import React from "react";
+import "./card.css";
 
 //this is a functional and presentational component
-const Card = (props) => (
-  <div className='card-container'>
-    <div className='card'>
-      <div className='front'>
-        <h2 className="text-center">Question </h2>
-        <div className='question'>{props.question}</div>
+const Card = props => (
+  <div className="card-container">
+    <div className="card">
+      <div className="front">
+        <div className="question">
+          <h2 className="text-center">
+            Question{" "}
+          </h2>
+          {props.question}
+        </div>
       </div>
-      <div className='back'>
-      <h2 className="text-center">Answer </h2>
-        <div className='answer'>{props.answer}</div>
+      <div className="back">
+        <div className="answer">
+          <h2 className="text-center">Answer </h2>
+          {props.answer}
+        </div>
       </div>
     </div>
   </div>
