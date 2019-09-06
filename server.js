9670132e288766325e9abde67c17c180154b4504
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-09-06 14:36:18 -0400

    background images

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/knodyacode",{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/knodenpyacode",{ useNewUrlParser: true });
 
 
 
