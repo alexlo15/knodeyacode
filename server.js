2886bf9ef0e2d3cff13d0f414b9bdcaad81d158f
Dealan <dealan.diongzon@gmail.com>
2019-09-08 12:36:49 -0400

    trying to deploy

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/knodeyacode",{ useNewUrlParser: true });
-
-
-
 
 // Start the API server
 app.listen(PORT, function() {
